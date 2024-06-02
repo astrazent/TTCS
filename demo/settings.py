7f2de6d2996cf71bbd6e15be55bb014a51740bca
astrazent <phannguyen2300@gmail.com>
2024-06-02 23:40:58 +0700

    TTCS

--- demo/settings.py
+++ demo/settings.py
@@ -68,8 +68,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, 'db.sqlite3')
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'ecommerce_app',
+        "USER": 'root',
+        "PASSWORD": '123456',
+        "HOST": 'localhost',
+        'PORT': '3307',
     }
 }
 
